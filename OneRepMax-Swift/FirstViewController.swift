1941dd853fc7eb96801ae36aff3468488c70a39d
Alim Charaniya <alimcharaniya@alims-air.gateway.pace.com>
2015-01-13 22:28:52 -0800

    Initial Commit

---
+++ OneRepMax-Swift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OneRepMax-Swift
+//
+//  Created by Alim Charaniya on 1/13/15.
+//  Copyright (c) 2015 Alim Charaniya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OneRepMax-Swift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OneRepMax-Swift
+//
+//  Created by Alim Charaniya on 1/13/15.
+//  Copyright (c) 2015 Alim Charaniya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
